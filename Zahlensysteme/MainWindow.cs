9943c6999ba057be1e996f8181eb0306b0d45c9f
Alexander Hermann <alex.hermann@icloud.com>
2016-04-28 01:47:30 +0200

    some changes in xamarian

--- Zahlensysteme/MainWindow.cs
+++ Zahlensysteme/MainWindow.cs
@@ -14,4 +14,8 @@ public partial class MainWindow: Gtk.Window
 		Application.Quit();
 		a.RetVal = true;
 	}
+	protected void OnQuitActionActivated(object sender, EventArgs e)
+	{
+		Application.Quit();
+	}
 }
